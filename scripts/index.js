b3e60299e7d2b0f7c20d4a91d12c95ff176e3533
LaneLR <laneleerichardson@gmail.com>
2025-05-02 13:01:00 -0500

    changes to css

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ const navbar = document.querySelector('#moving-navbar')
 const logo = document.querySelector('#logo')
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 110) {
+    if (window.scrollY > 100) {
         navbar.classList.add('active')
     } else {
         navbar.classList.remove('active')
